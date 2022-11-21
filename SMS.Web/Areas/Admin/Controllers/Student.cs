a125cd1cd66abe266c1e62e15de51a243bc0e72b
you@example.com <you@example.com>
2022-11-21 08:03:42 +0800

    Added Student Areas

---
+++ SMS.Web/Areas/Admin/Controllers/Student.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS.Web.Areas.Admin.Controllers;
+
+public class Student : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Upsert()
+    {
+        return View();
+    }
+}
